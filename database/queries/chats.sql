43f0a0c1503a66c71726e65329c8a57e9e53cd41
fykyby <95149002+fykyby@users.noreply.github.com>
2024-06-14 02:55:01 +0200

    Add chat get and post

--- database/queries/chats.sql
+++ database/queries/chats.sql
@@ -5,3 +5,19 @@ INSERT INTO chats (
   $1
 )
 RETURNING id, name;
+
+-- name: CreateUsersChat :one
+INSERT INTO users_chats (
+  user_id,
+  chat_id
+) VALUES (
+  $1,
+  $2
+)
+RETURNING user_id, chat_id;
+
+-- name: GetUsersChat :one
+SELECT user_id, chat_id FROM users_chats WHERE user_id = $1 AND chat_id = $2;
+
+-- name: DeleteChat :exec
+DELETE FROM chats WHERE id = $1;
